b925d73144e3230919d81c369a0a1d989662aa09
Mark <mverleg.noreply@gmail.com>
2017-12-20 15:22:51 +0100

    binary encoding done

---
+++ src/main/python/flex_size_int/src/encode.py
@@ -0,0 +1,79 @@
+
+from math import log, floor, ceil
+from struct import Struct
+
+from src.main.python.flex_size_int.src.utils import bin_to_txt
+
+packer = Struct('!q')  #todo: not used
+
+def long_to_bin8(nr):
+	#todo: not used
+	nr =  int(nr)
+	bitcnt = int(floor(log(nr, 2) + 1))
+	fullbin = packer.pack(nr)
+	shortbin = fullbin[len(fullbin) - (bitcnt - 1) // 8 - 1:]
+	return bitcnt, shortbin
+
+
+def bin8_to_flex(cnt, bin):
+	#todo: not used
+	blockcnt = int(ceil(cnt / 7))
+	print(bin)
+	print(bin[-1] % 128)
+	print(cnt, blockcnt)
+
+
+tobit = Struct('!b')
+
+
+def long_to_flex(nr):
+	anr = abs(int(nr))
+	if (anr > 2**63):
+		raise NotImplementedError("numbers bigger than Long cannot be encoded yet, because bit-shift is not exact for such values (value: {})".format(nr))
+	# print(anr)
+	if anr == 0:
+		blockcnt = 1
+	else:
+		bitcnt = int(floor(log(anr, 2))) + 2
+		blockcnt = int(ceil(bitcnt / 7))
+	bytes = [None] * blockcnt
+	# All except first blocks have 7 bits
+	for blocknr in range(blockcnt - 1, 0, -1):
+		# print(blocknr, anr % 128, anr // 128)
+		bytes[blocknr] = tobit.pack(anr % 128)[0]
+		anr >>= 7
+	# First block has 6 or fewer bits
+	# print(0, anr % 64)
+	bytes[0] = tobit.pack(anr % 64)[0]
+	anr >>= 6
+	# Set the first bit of last byte to 1
+	bytes[-1] += 1 << 7
+	# Set the second bit of the first byte to 1 if negative
+	if nr < 0:
+		bytes[0] += 1 << 6
+	return bytes
+
+
+if __name__ == '__main__':
+	# print(bin_to_txt(long_to_bin8(int(2**35))[-1]))
+	# print(bin_to_txt(long_to_bin8(int(2**7))[-1]))
+	# print(bin_to_txt(long_to_bin8(int(2**8 - 1))[-1]))
+	# print(bin_to_txt(long_to_bin8(int(2**8))[-1]))
+	# print(bin_to_txt(long_to_bin8(int(25))[-1]))
+	# print('---')
+	# print(bin8_to_flex(*long_to_bin8(25)))
+	# print(bin8_to_flex(*long_to_bin8(int(2**(5*7-1)))))
+	# print(bin8_to_flex(*long_to_bin8(int(2**(5*7)))))
+	print(bin_to_txt(long_to_flex(25)))
+	print(bin_to_txt(long_to_flex(115)))
+	print(bin_to_txt(long_to_flex(-115)))
+	print(bin_to_txt(long_to_flex(0)))
+	print(bin_to_txt(long_to_flex(129)))
+	# print(720575940379279359995*128 + 92233720368547758079418%128 - 92233720368547758079418)
+	# print('>>' + str(92233720368547758079418%64))
+	print(bin_to_txt(long_to_flex(58)))
+	print(bin_to_txt(long_to_flex(-413177)))
+	# print(bin_to_txt(long_to_flex(92233720368547758079418)))
+	# print('10000110_00001101_00100000_00000000_00010001_01010001_01101001_00000011_00010110_00011100_00001010_11001010')
+
+
--- src/main/python/flex_size_int/src/utils.py
+++ src/main/python/flex_size_int/src/utils.py
@@ -24,9 +24,32 @@ def nr_to_bintxt(nr):
 	return ustxt
 
 
-print(nr_to_bintxt(25))
-print(nr_to_bintxt(115))
-print(nr_to_bintxt(922337213615477180794186))
+def bin_to_txt(bytes):
+	txt = ''
+	is_first = True
+	for byt in bytes:
+		if is_first:
+			is_first = False
+		else:
+			txt += '_'
+		txt += '{0:d}{1:d}{2:d}{3:d}{4:d}{5:d}{6:d}{7:d}'.format(
+			byt & 128 != 0,
+			byt & 64 != 0,
+			byt & 32 != 0,
+			byt & 16 != 0,
+			byt & 8 != 0,
+			byt & 4 != 0,
+			byt & 2 != 0,
+			byt & 1 != 0,
+		)
+	return txt
+
+
+if __name__ == '__main__':
+	#todo: remove
+	print(nr_to_bintxt(25))
+	print(nr_to_bintxt(115))
+	print(nr_to_bintxt(922337213615477180794186))
 
 
 
