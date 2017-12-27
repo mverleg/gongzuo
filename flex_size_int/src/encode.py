a824811f23c17b02f656ead499ecd7a806b08bf7
Mark <mverleg.noreply@gmail.com>
2017-12-27 21:23:51 +0100

    make shifted encoding work

--- flex_size_int/src/encode.py
+++ flex_size_int/src/encode.py
@@ -6,59 +6,76 @@ from utils import DATAPERBYTE, NEGATIVE, LASTBYTE, BitShift
 tobit = Struct('!b')
 
 
-def long_to_flex(nr):
-	anr = abs(int(nr))
-	if (anr > 2**63):
-		raise NotImplementedError("numbers bigger than Long cannot be encoded yet, because bit-shift is not exact for such values (value: {})".format(nr))
-	if anr == 0:
-		return [LASTBYTE]
-	bitcnt = int(floor(log(anr, 2))) + 2
-	blockcnt = int(ceil(bitcnt / 7))
-	bytes = [None] * blockcnt
-	# All except first blocks have 7 bits
-	for blocknr in range(blockcnt - 1, 0, -1):
-		bytes[blocknr] = tobit.pack(anr % DATAPERBYTE)[0]
-		anr >>= 7
-	# First block has 6 or fewer bits
-	bytes[0] = tobit.pack(anr % 64)[0]
-	anr >>= 6  # TODO: doesn't actually get used, right?
-	# Set the first bit of last byte to 1
-	bytes[-1] += LASTBYTE
+def long_to_flex(nr, shift=None):
+	if shift is None:
+		shift = BitShift(0, [0])
+	assert isinstance(shift, BitShift)
+	# Create the flex-integer bytes
+	bytes = _convert_ulong_to_flex_with_offset(abs(int(nr)), shift.amt + 1, left_padding=1)
 	# Set the second bit of the first byte to 1 if negative
 	if nr < 0:
 		bytes[0] += NEGATIVE
+	# Add the data from the shift to the bytes
+	bytes = _combine_shiftdata_and_flexint(shift.data, bytes)
 	return bytes
 
 
 def ulong_to_flex(nr, shift=None):
 	if shift is None:
-		shift = BitShift(0, 0)
+		shift = BitShift(0, [0])
 	assert isinstance(shift, BitShift)
 	assert nr >= 0, "this function is for unsigned numbers"
+	# Create the flex-integer bytes
+	bytes = _convert_ulong_to_flex_with_offset(nr, shift.amt)
+	# Add the data from the shift to the bytes
+	bytes = _combine_shiftdata_and_flexint(shift.data, bytes)
+	## for bytenr, shiftdata in enumerate(shift.data):
+	## 	bytes[bytenr] = shiftdata if bytes[bytenr] is None else bytes[bytenr] + shiftdata
+	return bytes
+
+
+def _convert_ulong_to_flex_with_offset(nr, offset, left_padding=0):
+	"""
+	Code shared between `long_to_flex` and `ulong_to_flex`.
+	
+	:param nr: The positive number to convert to flex bytes format.
+	:param offset: The offset, including shift-data and negative sign, but without continuations.
+	:param left_padding: How many spaces to leave between the continuation mark and the data (e.g. for the sign).
+	"""
 	if (nr > 2**63):
 		raise NotImplementedError("numbers bigger than Long cannot be encoded yet, because bit-shift is not exact for such values (value: {})".format(nr))
+	assert nr >= 0
 	# Find how many blocks are needed for the shift, data and bookkeeping
-	bytecount = _determine_byte_count(1 + shift.amt, nr)
+	bytecount = _determine_byte_count(nr, offset)
 	bytes = [None] * bytecount
 	# All except first flexnr blocks have 7 bits
-	first_block_index = shift.amt // 8
+	first_block_index = offset // 8
 	for blocknr in range(bytecount - 1, first_block_index, -1):
 		bytes[blocknr] = tobit.pack(nr % DATAPERBYTE)[0]
 		nr >>= 7
 	# First flexnr block has 6 or fewer bits
-	first_block_nondata = (shift.amt % 8) + 1  # TODO: +2 for signed
+	first_block_nondata = (offset % 8) + 1
 	assert nr < 2**(8 - first_block_nondata), "remaining data is too large for first byte's size (this is a bug)"
 	# Shift the data to occupy the
-	bytes[0] = tobit.pack(nr)[0] << (first_block_nondata - 1)
+	bytes[0] = tobit.pack(nr)[0] << (first_block_nondata - 1 - left_padding)
 	# Set the first bit of last byte to 1
 	bytes[-1] = LASTBYTE if bytes[-1] is None else bytes[-1] + LASTBYTE
-	# Add the data from the shift to the bytes
-	for bytenr, shiftdata in enumerate(shift.data):
-		bytes[bytenr] = shiftdata if bytes[bytenr] is None else bytes[bytenr] + shiftdata
 	return bytes
 
 
-def _determine_byte_count(fixed_data_bits, numerical_value):
+def _combine_shiftdata_and_flexint(shift_data_bytes, flex_int_bytes):
+	"""
+	Combine the shift data and the flex int data into a single list of bytes.
+	
+	:param shift_data: Data to move, with length equal to the minimum bytes for the data, with the last byte right-aligned.
+	:param flex_int_bytes: Data representing the flex int, with empty values already padded to the left for the shift data. Changed in-place.
+	"""
+	for bytenr, shiftdata in enumerate(shift_data_bytes):
+		flex_int_bytes[bytenr] = shiftdata if flex_int_bytes[bytenr] is None else flex_int_bytes[bytenr] + shiftdata
+	return flex_int_bytes
+
+
+def _determine_byte_count(numerical_value, fixed_data_bits):
 	"""
 	Determine how many bytes exactly are needed to store a number along with some fixed/bookkeeping data,
 	using 7 data bits and one continuation bit per byte for the numerical value, and 8 bits per byte for other data.
@@ -67,10 +84,11 @@ def _determine_byte_count(fixed_data_bits, numerical_value):
 	:param numerical_value: The number to be stored in flexible size format.
 	"""
 	if (numerical_value == 0):
-		number_bits = 0
+		number_bits = 1
 	else:
-		number_bits = int(ceil(log(abs(numerical_value), 2)))
-	continuation_bits = int(ceil(number_bits / 7.))
+		# Use this instead of logarithm because logarithm has rounding errors for large numbers.
+		number_bits = abs(numerical_value).bit_length()
+	continuation_bits = int(floor(number_bits / 7.))
 	return int(ceil((number_bits + continuation_bits + fixed_data_bits) / 8.))
 
 
